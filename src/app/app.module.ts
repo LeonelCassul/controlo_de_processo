f605dea7c74adb79c355f4a50869ebe6c113fbfb
Leonel Cassul <leonelsalmos@gmail.com>
2023-04-28 22:45:01 +0100

    Configuraçao do projecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastarComponent } from './cadastar/cadastar.component';
+import { ProcessoComponent } from './processo/processo.component';
+import { HomeComponent } from './home/home.component';
+import { ConsultarComponent } from './consultar/consultar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastarComponent,
+    ProcessoComponent,
+    HomeComponent,
+    ConsultarComponent
   ],
   imports: [
     BrowserModule,
